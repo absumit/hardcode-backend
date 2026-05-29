8ed14e749e062dc7c930cdc1ddf60440b1aeca26
absumit <sugarfree7970@gmail.com>
2026-05-29 17:42:12 +0530

    / response

--- src/index.js
+++ src/index.js
@@ -27,6 +27,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hardcode backend is running' });
+});
+
 app.use('/user', authrouter)
 app.use('/problem',problemroutes)
 app.use('/solution',submitroutes)
